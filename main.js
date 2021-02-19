1b091ae147a546d5ad953c8aa896f3ae558bd579
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-02-18 18:46:56 -0600

    set up main js variables

---
+++ main.js
@@ -0,0 +1,10 @@
+//main variables to grab from settings
+
+const resultElement = document.getElementById('result');
+const lenghtElement = document.getElementById('length');
+const upperCaseElement = document.getElementById('uppercase');
+const lowerCaseElement = document.getElementById('lowercase');
+const numbersElement = document.getElementById('numbers');
+const symbolsElement = document.getElementById('symbols');
+const generateElement = document.getElementById('generate');
+const clipElement = document.getElementById('clipboard')
